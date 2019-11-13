5e152c7b75a8ee14f7e2a6ac5bcf72eac242a115
christsona <31924411+christsona@users.noreply.github.com>
2019-11-12 22:52:17 -0500

    Update color.js

--- color.js
+++ color.js
@@ -1,6 +1,6 @@
 function changeColor(){
     if (document.getElementById("whoweare").style.color != "yellow"){
-        document.getElementById("whoweare").style.color = ("yellow");
+        document.getElementById("whoweare").style.color = ("orange");
     }
     else{
         document.getElementById("whoweare").style.color = ("black");
